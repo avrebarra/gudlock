c1045d3b44cd0a5f9682b5018df83b7bf3681452
shrotavre <avreghly@gmail.com>
2019-09-11 16:08:53 +0700

    Setup server code

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const net = require('net')
+const locks = require('./utils/locks')
+
+const socketServer = net.createServer()
+
+const formatLockName = (lockName) => lockName.replace(' ', '_')
+
+socketServer.on('connection', (socket) => {
+  socket.on('data', (data) => {
+    // valid message format is case-sensitive texts splitted with '#' char.
+    // First part will be the command name, second part will be the params (lock key name)
+    // Example: LOCK#NEW_REQUEST_REGISTER or RELEASE#NEW_REQUEST_REGISTER
+    const [command, params] = data.toString().trim().split('#')
+    console.log(command, params);
+
+    var lockName, passphrase
+
+    switch (command.toUpperCase()) {
+      case 'LOCK':
+        lockName = params
+        socket.write(locks.acquireLock(formatLockName(lockName)) + '\n')
+        break;
+
+      case 'RELEASE':
+        [lockName, passphrase] = params.split(':')
+        socket.write(locks.releaseLock(formatLockName(lockName), passphrase) + '\n')
+        break;
+    }
+  })
+})
+
+module.exports = socketServer
---
+++ server/utils/locks.js
@@ -0,0 +1,22 @@
+const lockRegistry = {}
+
+const acquireLock = (lockname) => {
+  if (lockRegistry[lockname]) return 'FAIL'
+
+  // generate lock passphrase from timestamp and register
+  const lock = (+new Date).toString(36)
+  lockRegistry[lockname] = lock
+
+  return `OK#${lock}`
+}
+
+const releaseLock = (lockname, passphrase) => {
+  if (!lockRegistry[lockname]) return 'OK#NO_CHANGE'
+  if (lockRegistry[lockname] && lockRegistry[lockname] != passphrase) return 'FAIL'
+
+  lockRegistry[lockname] = null
+
+  return 'OK'
+}
+
+module.exports = { acquireLock, releaseLock }
