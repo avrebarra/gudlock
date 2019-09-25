af8ed904065886c2e6de6ec2504644f63c522ea1
Avreghly Barra <avreghly@gmail.com>
2019-09-25 16:47:43 +0700

    Update documentation

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ socketServer.on('connection', (socket) => {
   socket.on('data', (data) => {
     // valid message format is case-sensitive texts splitted with '#' char.
     // First part will be the command name, second part will be the params (lock key name)
-    // Example: LOCK#NEW_REQUEST_REGISTER or RELEASE#NEW_REQUEST_REGISTER
+    // Example: LOCK#NEW_REQUEST_REGISTER or RELEASE#NEW_REQUEST_REGISTER:PASS_PHRASE
     const [command, params] = data.toString().trim().split('#')
     console.log(command, params)
 
